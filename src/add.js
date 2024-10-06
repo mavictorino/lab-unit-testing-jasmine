d4021feb2c81ef5ccc61b0960535c67d8736ddf4
Mariana Victorino <victorino.marii@gmail.com>
2024-10-06 19:04:44 +0100

    lab done

--- src/add.js
+++ src/add.js
@@ -5,3 +5,18 @@ function add (numOne, numTwo) {
 
     return numOne + numTwo;
 }
+
+/* 1. checks if the function is defined --- the test passes.
+2. checks if function takes two parameters (numOne, numTwo) --- satisfies the test.
+3. checks if function correctly returns the sum of two numbers --- pass.
+4. returns undefined if any argument is missing --- pass. */
+
+// Updating the function to pass the new test: 
+
+function add(numOne, numTwo) {
+    if (typeof numOne !== 'number' || typeof numTwo !== 'number') {
+      return undefined;
+    }
+    return numOne + numTwo;
+  }
+  
--- tests/add.spec.js
+++ tests/add.spec.js
@@ -19,6 +19,33 @@ describe("Iteration 1 | Getting Started", () => {
         expect(add()).toEqual(undefined);
         expect(add(undefined, 1)).toEqual(undefined);
       });
+
+      it("should return undefined if any of the two arguments is not a number", () => {
+        expect(add(1, "2")).toEqual(undefined); 
+        expect(add("1", 2)).toEqual(undefined); 
+        expect(add(true, 2)).toEqual(undefined); 
+        expect(add(1, {})).toEqual(undefined); 
+        expect(add(1, [])).toEqual(undefined); 
+      });
     });
   });
+   
   
+ // **Review the Tests**
+
+ //  1. How many tests are there for the `add` function in the `add.spec.js` file?
+ //there are four tests: 1. if `add` is defined | if `add` takes two arguments | if returns the sum of the two numbers | if returns undefined if any of the arguments are not provided 
+  
+  
+ // 2. How are the blocks `describe` and `it` being used in the tests? What is the purpose of each?
+ // in "describe" we have the tests related to the Iteration in question. the "it" blocks describes specifics functionalities that are expected from the function add. 
+  
+  
+ // 3. How are the test descriptions phrased? Are there any keywords that stand out?
+ // using should, to be defined and to equal, they are descriptive, describing what the function should do in specifics conditions. 
+  
+  
+ // 4. What do the `expect` functions do, and what input do they take?
+ // they check if the final outcome matches the expected outcome.
+
+ 
